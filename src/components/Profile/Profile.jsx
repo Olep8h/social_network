3ecea8997d1875e002518ab6663bb7964094bcd5
Olep8h <panov@student.vspj.cz>
2022-11-20 17:07:15 +0200

    folder structure new components

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from './Profile.module.css'
+import MyPosts from "./MyPosts/MyPosts";
+
+
+const Profile = () => {
+    return (
+    <div className={classes.content}>
+        <div>
+            <img src='https://static.trip101.com/paragraph_media/pictures/002/412/765/large/1200px-Cmglee_Cambridge_Trinity_College_Great_Court.jpg?1617627235' />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <MyPosts/>
+    </div>
+)
+}
+
+export default Profile;
