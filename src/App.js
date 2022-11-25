241e8602692125fdad13bfb48c43cddd2bd2a6ef
Olep8h <panov@student.vspj.cz>
2022-11-25 11:10:20 +0200

    fix routing

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const App = (props) => {
   return (
-      <Router>
-          <Routes>
-  <div className='app-wrapper'>
+
+      <div className='app-wrapper'>
       <Header/>
       <Navbar/>
       <div className='app-wrapper-content'>
-          <Route path='/dialogs' element={Dialogs}/>
-          <Route path='/profile' element={Profile}/>
-          <Route path='/header' element={Header}/>
-          <Route path='/navbar' element={Navbar}/>
+          <Routes>
+          <Route path='/dialogs' element={<Dialogs />}/>
+          <Route path='/profile' element={<Profile />}/>
+          <Route path='/header' element={<Header />}/>
+          <Route path='/navbar' element={<Navbar />}/>
+      </Routes>
       </div>
   </div>
-          </Routes>
-      </Router>
+
   )
 }
 
