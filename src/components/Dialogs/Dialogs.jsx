39553277278f0722fba8c20fb5f22b50c6cc535f
Olep8h <panov@student.vspj.cz>
2022-11-26 04:32:52 +0200

    start work with Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,31 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>
+                    Oleh
+                </div>
+                <div className={classes.dialog + ' ' + classes.act}>
+                    Alex
+                </div>
+                <div className={classes.dialog}>
+                    Ann
+                </div>
+                <div className={classes.dialog}>
+                    Maks
+                </div>
+                <div className={classes.dialog}>
+                    Lisa
+                </div>
+            </div>
+                <div className={classes.messages}>
+                    <div className={classes.message}>Hello</div>
+                    <div className={classes.message}>So you can?</div>
+                    <div className={classes.message}>Actually no..</div>
+                    <div className={classes.message}>Yep i do this</div>
+                    <div className={classes.message}>Of course! </div>
+            </div>
         </div>
     )
 }
