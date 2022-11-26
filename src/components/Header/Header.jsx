a10f02a74faf236d2a588c5efbd6ac9943ee4901
Olep8h <panov@student.vspj.cz>
2022-11-26 04:31:14 +0200

    different options of logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,10 @@ import classes from './Header.module.css';
 const Header = () => {
     return <header className={classes.header}>
         <img src='https://png.pngitem.com/pimgs/s/32-326322_linkedin-logo-white-letters-png-download-transparent-png.png'/>
+        {/*https://c0.klipartz.com/pngpicture/358/320/gratis-png-testimonios-informaticos-iconos-cliente-prestashop-icono-mensaje-s-thumbnail.png*/}
+        {/*https://c0.klipartz.com/pngpicture/463/152/gratis-png-iconos-informaticos-testimoniales-publicidad-promocion-servicio-comunicacion-thumbnail.png*/}
+        {/* https://w7.pngwing.com/pngs/96/218/png-transparent-social-media-marketing-digital-marketing-person-information-support-team-logo-social-media-marketing-social-media.png*/}
+        {/*https://c0.klipartz.com/pngpicture/358/320/gratis-png-testimonios-informaticos-iconos-cliente-prestashop-icono-mensaje-s-thumbnail.png*/}
     </header>
 }
 
