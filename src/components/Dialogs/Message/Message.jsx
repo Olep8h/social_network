4bd6dd17135af782a1a4d0d68c4f98308d0c6f6e
Olep8h <panov@student.vspj.cz>
2022-12-02 23:01:00 +0200

    create onClick, ref for Messages

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -2,10 +2,28 @@ import React from "react";
 import classes from "./Message.module.css";
 
 
+let newMessageElement = React.createRef();
+
+let addMessage = () => {
+    let text = newMessageElement.current.value;
+    alert(text);
+
+}
+
 const Message = (props) => {
     return (
-        <div className={classes.message}>{props.message}</div>
+        <div
+            className={classes.message}>{props.message}
+            <div>
+                <textarea className={classes.textMessage} ref={newMessageElement}></textarea>
+            </div>
+            <div>
+                <button className={classes.addMessageButton} onClick={addMessage}>Reply to message...
+                </button>
+            </div>
+        </div>
     )
+
 }
 
 export default Message;
