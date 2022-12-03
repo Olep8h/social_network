1a5195a43dda67dfa0966035a5081c95d645408f
Olep8h <panov@student.vspj.cz>
2022-12-03 02:01:07 +0200

    move callback through props

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ const App = (props) => {
                     <Route path='/dialogs'
                            element={<Dialogs state={props.state.dialogsPage}/>}/>
                     <Route path='/profile'
-                           element={<Profile state={props.state.profilePage}/>}/>
+                           element={<Profile state={props.state.profilePage}
+                                             addPost={props.addPost}/>}/>
                 </Routes>
             </div>
         </div>
