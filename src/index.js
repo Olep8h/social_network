dc82092339e0dfa7b97713abfee4d5f517246c89
Olep8h <panov@student.vspj.cz>
2022-12-03 02:00:25 +0200

    create file for render
    FLUX rule

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {renderEntireTree} from "./render";
 import state from "./redux/state";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <BrowserRouter>
-    <App state={state} />
-      </BrowserRouter>
-  </React.StrictMode>
-);
 
+renderEntireTree(state);
 
