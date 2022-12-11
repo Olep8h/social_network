f7fed791d883cbe4327586d6ee1c8a6bc21a90f1
Olep8h <panov@student.vspj.cz>
2022-12-12 01:19:49 +0200

    change imports

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import {Routes, Route} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import './App.css';
-import store from "./redux/store";
+
 
 
 const App = (props) => {
@@ -17,11 +17,9 @@ const App = (props) => {
             <div className='app-wrapper-content'>
                 <Routes>
                     <Route path='/dialogs'
-                           element={<Dialogs store={props.store} />}/>
+                           element={<DialogsContainer store={props.store} />}/>
                     <Route path='/profile'
-                           element={<Profile profilePage={props.state.profilePage}
-                                             dispatch={props.dispatch}
-                                             />}/>
+                           element={<Profile store={props.store} />}/>
                 </Routes>
             </div>
         </div>
