3d2b585790a6bff125d0a3e61e4caebe499103dc
Olep8h <panov@student.vspj.cz>
2022-12-12 01:15:05 +0200

    clearing data and state management
    now it's work for MyPostsContainer

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,28 +1,23 @@
 import React from "react";
 import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
-import {addPostActionCreator, onPostChangeActionCreator} from "../../../redux/profile_reducer";
-
 
 
 
 const MyPosts = (props) => {
+    debugger;
     let postsElements = props.postsData
         .map((post) => <Post message={post.message} likesCount={post.likesCount}/>)
 
     let newPostElement = React.createRef();
 
-    let addPost = () => {
-        //props.addPost();
-        props.dispatch(addPostActionCreator());
+    let onAddPost = () => {
+        props.addPost();
     }
 
     let onPostChange = () =>  {
         let text = newPostElement.current.value;
-        //props.updateNewPostText(text);
-        // let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text};
-        let action = onPostChangeActionCreator(text);
-        props.dispatch(action);
+        props.updateNewPostText(text);
     }
 
     return (
@@ -30,11 +25,13 @@ const MyPosts = (props) => {
             <h2 className={classes.titleMyPhotos}>My photos</h2>
             <div>
                 <div>
-                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
+                    <textarea onChange={onPostChange}
+                              ref={newPostElement}
+                              value={props.newPostText}/>
                 </div>
                 <div>
-                    <button className={classes.addPostButton} onClick={addPost}>Add Post
-                    </button>
+                    <button className={classes.addPostButton}
+                            onClick={onAddPost}>Add Post</button>
                 </div>
             </div>
             <div className={classes.posts}>
