b81f9c1ecb916590204663686dff561aef81ddae
Olep8h <panov@student.vspj.cz>
2022-12-11 15:09:30 +0200

    import redux-store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
-import store from "./redux/state";
+import store from "./redux/redux-store";
 import './index.css';
 import App from './App';
 
