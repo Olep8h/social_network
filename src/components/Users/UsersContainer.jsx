1b613a038e976ff48259de667b28005f70af7f90
Olep8h <panov@student.vspj.cz>
2022-12-20 00:30:00 +0200

    export default without creating a variable UsersContainer

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -27,6 +27,5 @@ let mapDispatchToProps = (dispatch) => {
 
 
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
 
-export default UsersContainer;
