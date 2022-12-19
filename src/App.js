084f2bb12064e7d5390cfd4b36f9fa3480b8e57c
Olep8h <panov@student.vspj.cz>
2022-12-19 20:55:24 +0200

    routing for users

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 import './App.css';
+
 
 
 
@@ -20,6 +22,8 @@ const App = (props) => {
                            element={<DialogsContainer/>}/>
                     <Route path='/profile'
                            element={<Profile/>}/>
+                    <Route path='/users'
+                           element={<UsersContainer/>}/>
 
                 </Routes>
             </div>
