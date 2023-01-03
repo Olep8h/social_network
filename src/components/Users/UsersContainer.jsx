db3c600d0e973d958bcae4c1a43fb6324bd2edb2
Olep8h <panov@student.vspj.cz>
2023-01-03 23:41:57 +0100

    work with isLoader  in the user container component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../redux/users_reducer";
+import {
+    followAC,
+    isLoadingAC,
+    setCurrentPageAC,
+    setTotalUsersCountAC,
+    setUsersAC,
+    unfollowAC
+} from "../../redux/users_reducer";
 import {connect} from "react-redux";
 import axios from "axios";
 import Users from "./Users";
+import preloader from "../../assets/images/preloader.gif";
+import styles from "./Users.module.css";
 
 class UsersContainer extends React.Component {
     componentDidMount() {
+        this.props.isLoading(true);
         axios.get("https://social-network.samuraijs.com/api/1.0/users?count=" + this.props.pageSize + "&page=" + this.props.currentPage)
             .then(response => {
+                this.props.isLoading(false);
                 this.props.setUsers(response.data.items);
                 this.props.setTotalUsersCount(response.data.totalCount);
             });
@@ -15,22 +26,27 @@ class UsersContainer extends React.Component {
 
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber);
+        this.props.isLoading(true);
         axios.get("https://social-network.samuraijs.com/api/1.0/users?count=" + this.props.pageSize + "&page=" + pageNumber)
             .then(response => {
+                this.props.isLoading(false);
                 this.props.setUsers(response.data.items);
             });
     }
 
     render() {
-        return <Users
-            totalUsersCount={this.props.totalUsersCount}
-            pageSize={this.props.pageSize}
-            currentPage={this.props.currentPage}
-            onPageChanged={this.onPageChanged}
-            users={this.props.users}
-            follow={this.props.follow}
-            unfollow={this.props.unfollow}
-        />
+        return <>
+            {this.props.isLoading ? <img src={preloader} className={styles.preloader}/> : null}
+            <Users
+                totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChanged={this.onPageChanged}
+                users={this.props.users}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+            />
+        </>
     }
 }
 
@@ -40,7 +56,8 @@ let mapStateToProps = (state) => {
         users: state.usersPage.usersData,
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage
+        currentPage: state.usersPage.currentPage,
+        isLoading: state.usersPage.isLoading
     }
 }
 
@@ -56,14 +73,16 @@ let mapDispatchToProps = (dispatch) => {
             dispatch(setUsersAC(users))
         },
         setCurrentPage: (pageNumber) => {
-            dispatch (setCurrentPageAC(pageNumber))
+            dispatch(setCurrentPageAC(pageNumber))
         },
         setTotalUsersCount: (totalCount) => {
-            dispatch (setTotalUsersCountAC(totalCount))
+            dispatch(setTotalUsersCountAC(totalCount))
+        },
+        isLoading: (isLoading) => {
+            dispatch(isLoadingAC(isLoading))
         }
     }
 }
-
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
