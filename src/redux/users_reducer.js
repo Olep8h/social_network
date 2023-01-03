e5f1a937470939eceaa468b85bd2a02ccaf3e292
Olep8h <panov@student.vspj.cz>
2023-01-03 23:40:00 +0100

    state management for isLoading

--- src/redux/users_reducer.js
+++ src/redux/users_reducer.js
@@ -3,12 +3,14 @@ const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
+const IS_LOADING = 'IS_LOADING'
 
 let initialState = {
     usersData: [],
     pageSize: 5,
     totalUsersCount: 0,
     currentPage: 1,
+    isLoading: true
 };
 
 
@@ -43,6 +45,9 @@ const usersReducer = (state = initialState, action) => {
         case SET_TOTAL_USERS_COUNT: {
             return {...state, totalUsersCount: action.count}
         }
+        case IS_LOADING: {
+            return {...state, isLoading: action.isLoading}
+        }
 
         default:
             return state;
@@ -54,5 +59,6 @@ export const unfollowAC = (userId) => ({type: UNFOLLOW, userId})
 export const setUsersAC = (users) => ({type: SET_USERS, users})
 export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
 export const setTotalUsersCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount})
+export const isLoadingAC = (isLoading) => ({type: IS_LOADING, isLoading})
 
 export default usersReducer;
