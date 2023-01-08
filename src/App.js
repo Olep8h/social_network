acdb9d31a2e350c71980f053f2e6203307fa670b
Olep8h <panov@student.vspj.cz>
2023-01-08 17:58:21 +0200

    Route to ProfileContainer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import {Routes, Route} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 import './App.css';
 
 
@@ -21,7 +21,7 @@ const App = (props) => {
                     <Route path='/dialogs'
                            element={<DialogsContainer/>}/>
                     <Route path='/profile'
-                           element={<Profile/>}/>
+                           element={<ProfileContainer/>}/>
                     <Route path='/users'
                            element={<UsersContainer/>}/>
 
