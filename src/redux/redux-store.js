557d1d4ad28a03ea4c478927274d8129df12ca49
Olep8h <panov@student.vspj.cz>
2023-01-11 01:18:53 +0200

    connection authentication_reducer to our store

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import dialogsReducer from "./dialogs_reducer";
 import profileReducer from "./profile_reducer";
 import sidebarReducer from "./sidebar_reducer";
 import users_reducer from "./users_reducer";
+import authenticationReducer from "./authentication_reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: users_reducer,
+    authentication: authenticationReducer,
 });
 
 let srote = createStore(reducers);
