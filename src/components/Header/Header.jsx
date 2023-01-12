c8cd7b1af82bba681913938afa60138c09e28d7b
Olep8h <panov@student.vspj.cz>
2023-01-12 14:51:44 +0200

    rendering a login block

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import classes from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
 
-const Header = () => {
+const Header = (props) => {
     return <header className={classes.header}>
-        <img src='https://png.pngitem.com/pimgs/s/32-326322_linkedin-logo-white-letters-png-download-transparent-png.png'/>
+        <img
+            src='https://png.pngitem.com/pimgs/s/32-326322_linkedin-logo-white-letters-png-download-transparent-png.png'/>
         {/*https://c0.klipartz.com/pngpicture/358/320/gratis-png-testimonios-informaticos-iconos-cliente-prestashop-icono-mensaje-s-thumbnail.png*/}
         {/*https://c0.klipartz.com/pngpicture/463/152/gratis-png-iconos-informaticos-testimoniales-publicidad-promocion-servicio-comunicacion-thumbnail.png*/}
         {/* https://w7.pngwing.com/pngs/96/218/png-transparent-social-media-marketing-digital-marketing-person-information-support-team-logo-social-media-marketing-social-media.png*/}
         {/*https://c0.klipartz.com/pngpicture/358/320/gratis-png-testimonios-informaticos-iconos-cliente-prestashop-icono-mensaje-s-thumbnail.png*/}
+        <div className={classes.login}>
+            {props.isAuth ? props.login : <NavLink
+                to={'/login'}
+                style={{textDecoration: 'none'}}>Login</NavLink>}
+        </div>
     </header>
 }
 
