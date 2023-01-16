1634b44f18aaff7752cff006e3b4d220cd78fea1
Olep8h <panov@student.vspj.cz>
2023-01-16 17:13:25 +0200

    + thunkMiddleware

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,9 +1,10 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import dialogsReducer from "./dialogs_reducer";
 import profileReducer from "./profile_reducer";
 import sidebarReducer from "./sidebar_reducer";
 import users_reducer from "./users_reducer";
 import authenticationReducer from "./authentication_reducer";
+import thunkMiddleware from "redux-thunk";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -13,7 +14,9 @@ let reducers = combineReducers({
     auth: authenticationReducer,
 });
 
-let srote = createStore(reducers);
+
+
+let srote = createStore(reducers, applyMiddleware(thunkMiddleware));
 window.store = srote;
 
 export default srote;
