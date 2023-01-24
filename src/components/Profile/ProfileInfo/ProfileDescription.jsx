cb1f1bb0bd8b5c0ee34def7050e64d1e0e492579
Olep8h <panov@student.vspj.cz>
2023-01-24 19:10:22 +0200

    Implementation of status display and update

--- src/components/Profile/ProfileInfo/ProfileDescription.jsx
+++ src/components/Profile/ProfileInfo/ProfileDescription.jsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const ProfileDescription = (props) => {
-    const [editMode, setEditMode] = React.useState(false);
+    const [editMode, setEditMode] = useState(false);
+    const [status, setStatus] = useState(props.status);
+
+    useEffect(() => {
+        setStatus(props.status);
+    }, [props.status]);
 
     const activateEditMode = () => {
         setEditMode(true);
     }
+
     const deactivateEditMode = () => {
         setEditMode(false);
+        props.updateStatus(status);
+    }
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
     }
 
     return (
         <div>
             {!editMode &&
                 <div>
-                    <span onDoubleClick={activateEditMode}>{props.status}</span>
+                    <span onDoubleClick={activateEditMode}>{props.status || "No status.."}</span>
                 </div>
             }
             {editMode &&
                 <div>
-                    <input autoFocus={true} onBlur={deactivateEditMode} value={props.status}/>
+                    <input onChange={onStatusChange}
+                           autoFocus={true}
+                           onBlur={deactivateEditMode}
+                           value={status}/>
                 </div>
             }
         </div>
