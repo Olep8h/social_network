36cb1457caaee459ab30adea5494da4de1ad8c40
Olep8h <panov@student.vspj.cz>
2023-01-24 19:03:29 +0200

    + server requests (getProfileStatus, updateProfileStatus)

--- src/api/api.js
+++ src/api/api.js
@@ -28,18 +28,41 @@ export const usersAPI = {
                 .then(response => {
                     return response.data;
                 })
-        )},
+        )
+    },
+    getProfileInfo(profileId) {
+
+        console.warn('Obsolete method. Please use profileAPI object')
+        return (
+            profileAPI.getProfileInfo(profileId)
+        )
+    }
+}
+
+export const profileAPI = {
     getProfileInfo(profileId) {
         return (
-            instance.get( `profile/`+ profileId)
-        )}
+            instance.get(`profile/` + profileId)
+        )
+    },
+    getProfileStatus(profileId) {
+        return (
+            instance.get(`profile/status/` + profileId)
+        )
+    },
+    updateProfileStatus(status) {
+        return (
+            instance.put(`profile/status`, {status: status})
+        )
+    }
 }
 
 export const loginAPI = {
     me() {
         return (
             instance.get(`auth/me`)
-        )}
+        )
+    }
 }
 
 
