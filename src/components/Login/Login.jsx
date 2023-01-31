917bb48e5a2671e0f67feaa79b34aa8fafb61ee4
Olep8h <panov@student.vspj.cz>
2023-01-31 17:06:55 +0200

    сreating a login with a local state

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+
+const LoginForm = (props) => {
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [rememberMe, setRememberMe] = useState(false);
+    return (
+        <form onSubmit={(e) => { e.preventDefault(); console.log({ login, password, rememberMe });}}>
+            <div>
+                <input placeholder="Login" value={login} onChange={(e) => setLogin(e.target.value)} />
+            </div>
+            <div>
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <div>
+                <input type="checkbox" checked={rememberMe} onChange={() => setRememberMe(!rememberMe)} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
 
 const Login = (props) => {
     return (
-        <h1>Login</h1>
+        <div>
+            <h1>Login</h1>
+            <LoginForm />
+        </div>
     )
 }
 
 export default Login;
+
+// import React from "react";
+// import {Field, reduxForm} from "redux-form";
+//
+//
+// const LoginForm = (props) => {
+//     return (
+//             <form onSubmit={props.handleSubmit}>
+//                 <div>
+//                     <Field placeholder={"Login"} name={"login"} component={"input"}/>
+//                 </div>
+//                 <div>
+//                     <Field placeholder={"Password"} name={"password"} component={"input"}/>
+//                 </div>
+//                 <div>
+//                     <Field type={"checkbox"} name={"rememberMe"} component={"input"}/> remember me
+//                 </div>
+//                 <div>
+//                     <button>Login</button>
+//                 </div>
+//             </form>
+//     )
+// }
+//
+// const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
+//
+//
+// const Login = (props) => {
+//     const onSubmit = (formData) => {
+//         console.log(formData);
+//     }
+//     return (
+//         <div>
+//             <h1>Login</h1>
+//             <LoginReduxForm onSubmit={onSubmit}/>
+//         </div>
+//     )
+// }
+//
+// export default Login;
