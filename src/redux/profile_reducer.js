4bacd0c2ebf853d65cb7109850f076cad3a20856
Olep8h <panov@student.vspj.cz>
2023-02-06 21:51:00 +0200

    change reducer info
    now we don't need this info cuz use a local state

--- src/redux/profile_reducer.js
+++ src/redux/profile_reducer.js
@@ -1,7 +1,6 @@
 import {profileAPI, usersAPI} from "../api/api";
 
 const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
 
@@ -12,7 +11,6 @@ let initialState = {
         {id: 3, message: 'I want to create my own social network!!', likesCount: 22},
         {id: 4, message: 'I want pineapple pizza', likesCount: 25}
     ],
-    newPostText: 'inMoment.com',
     profile: null,
     status: ""
 };
@@ -23,19 +21,13 @@ const profileReducer = (state = initialState, action) => {
         case ADD_POST: {
             let newPost = {
                 id: 5,
-                message: state.newPostText,
+                message: action.newPostText,
                 likesCount: 0,
             };
             return {
                 ...state,
                 postsData: [...state.postsData, newPost],
                 newPostText: '',
-            };
-        }
-        case UPDATE_NEW_POST_TEXT: {
-            return {
-                ...state,
-                newPostText: action.newText
             };
         }
         case SET_USER_PROFILE: {
@@ -49,11 +41,9 @@ const profileReducer = (state = initialState, action) => {
     }
 }
 
-export const addPostActionCreator = () => ({type: ADD_POST})
+export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostText})
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
 export const setStatus = (status) => ({type: SET_STATUS, status})
-export const onPostChangeActionCreator = (text) => (
-    {type: UPDATE_NEW_POST_TEXT, newText: text})
 
 export const retrieveUserProfile = (profileId) => (dispatch) => {
     usersAPI.getProfileInfo(profileId)
