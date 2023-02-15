0386211a47788bd516502e5730cf21aea248a69e
Olep8h <panov@student.vspj.cz>
2023-02-15 12:54:08 +0200

    server requests login and logout in DAL

--- src/api/api.js
+++ src/api/api.js
@@ -62,6 +62,16 @@ export const loginAPI = {
         return (
             instance.get(`auth/me`)
         )
+    },
+    login(email, password, rememberMe = false) {
+        return (
+            instance.post(`auth/login`, {email, password, rememberMe})
+        )
+    },
+    logout() {
+        return (
+            instance.delete(`auth/login`)
+        )
     }
 }
 
