1bda80db46479440c6a4c85adc2cdafb61e2a1cc
Olep8h <panov@student.vspj.cz>
2023-03-01 11:15:06 +0200

    server request saveProfilePhoto
    DAL LEVEL

--- src/api/api.js
+++ src/api/api.js
@@ -54,6 +54,17 @@ export const profileAPI = {
         return (
             instance.put(`profile/status`, {status: status})
         )
+    },
+    saveProfilePhoto(photoFile) {
+        const formData = new FormData();
+        formData.append("image", photoFile);
+        return (
+            instance.put(`profile/photo`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+        )
     }
 }
 
