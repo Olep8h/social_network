6a40a7ea6297aedfbd16a99c8ed3af4855fa7c82
Olep8h <panov@student.vspj.cz>
2023-03-04 12:17:55 +0200

    + createField

--- src/components/common/FormControls/FormControls.js
+++ src/components/common/FormControls/FormControls.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./FormControls.module.css";
+import {Field} from "redux-form";
 
 const FormControl = ({input, meta, child, ...props}) => {
     const hasError = meta.touched && meta.error;
@@ -29,3 +30,13 @@ export const Input = (props) => {
             </FormControl>
     );
 }
+
+export const createField = (placeholder, name, validators, component, props = {}, text = "") => (
+    <div>
+        <Field placeholder={placeholder} name={name}
+                validate={validators}
+                component={component}
+                {...props}
+        /> {text}
+    </div>
+)
