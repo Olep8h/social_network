1bbbdecf85e0674c77cf98ab7adaeac453db801d
Olep8h <panov@student.vspj.cz>
2023-03-05 15:32:12 +0200

    create ProfileData
    The component displays the user's full name, about me, and whether they are looking for a job or not. If they are looking for a job, their professional skills are displayed as well. Also displayed user's contacts

---
+++ src/components/Profile/ProfileInfo/ProfileData.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+import classes from './ProfileData.module.css'
+
+const ProfileData = ({profile, isOwner, activeEditMode}) => {
+    return <div>
+        {isOwner && <div>
+            <button onClick={activeEditMode} className={classes.edit_button}>Edit</button>
+        </div>}
+        <div className={classes.full_name}>
+            <b>Full name</b>: {profile.fullName}
+        </div>
+        <div className={classes.about_me}>
+            <b>About me</b>: {profile.aboutMe}
+        </div>
+        <div className={classes.looking_for_job}>
+            <b>Looking for a job</b>: {profile.lookingForAJob ? "yes" : "no"}
+        </div>
+        {profile.lookingForAJob &&
+            <div className={classes.skills}>
+                <b>My professional skills</b>: {profile.lookingForAJobDescription}
+            </div>
+        }
+        <div className={classes.contacts_caption}>
+            <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
+            return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
+        })}
+        </div>
+    </div>
+}
+
+
+const Contact = ({contactTitle, contactValue}) => {
+    return <div className={classes.contacts}><b>{contactTitle}</b>: {contactValue}</div>
+}
+
+export default ProfileData;
