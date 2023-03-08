4d2d3bbc98925cb9a000a0101c27433649140696
Olep8h <panov@student.vspj.cz>
2023-03-08 12:49:02 +0200

    server request getCaptchaUrl

--- src/api/api.js
+++ src/api/api.js
@@ -79,14 +79,22 @@ export const loginAPI = {
             instance.get(`auth/me`)
         )
     },
-    login(email, password, rememberMe = false) {
+    login(email, password, rememberMe = false, captcha = null) {
         return (
-            instance.post(`auth/login`, {email, password, rememberMe})
+            instance.post(`auth/login`, {email, password, rememberMe, captcha})
         )
     },
     logout() {
         return (
             instance.delete(`auth/login`)
+        )
+    }
+}
+
+export const securityAPI = {
+    getCaptchaUrl() {
+        return (
+            instance.get(`security/get-captcha-url`)
         )
     }
 }
